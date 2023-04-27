1f549f38ef7696994bc3d47987d44fd17936ddfb
Wagner Castro <102252795+OliveerCastro@users.noreply.github.com>
2023-04-27 18:51:11 -0300

    models nodejs

---
+++ models/SeguidorModel.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const SeguidorSchema = new Schema({
+    // Quem segue
+    usuarioId : {type : String, required : true},
+    // Quem está seguindo
+    usuarioSeguidoId : {type : String, required : true}
+})
+
+export const SeguidorModel = (mongoose.models.seguidores ||
+    mongoose.model('seguidores', SeguidorSchema)
+)
